070aaf6b979229f08f80d68a9124a39b221e6d09
mahesabagusr <mahesabagusraditya1@gmail.com>
2025-01-29 17:29:46 +0700

    Merapihkan Materi

---
+++ Task/index.js
@@ -0,0 +1,32 @@
+let notes = [
+  {
+    id: 1,
+    heading: "Mempelajari Javascript",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus non expedita similique tempora tempore exercitationem. Accusamus sint porro iste quos velit aperiam voluptas placeat error nulla maxime id, deserunt eum.",
+    created_by: "John Doe",
+    created_at: 1738146027613
+  }
+]
+
+const createNote = () => {
+  //kode kalian
+}
+
+const readNote = () => {
+  //kode kalian
+}
+
+const updateNote = () => {
+  // kode kalian
+}
+
+const deleteNote = () => {
+  //kode kalian
+}
+
+// mengetes kode : 
+
+createNote("Belajar React", "Mempelajari dasar-dasar React dan cara membuat komponen.", "Jane Doe")
+readNote();
+updateNote(0, "Mempelajari JavaScript Lanjutan", "Mempelajari konsep lanjutan dalam JavaScript.");
+readNote();
